5676670ad4cac50fa2b03abfa4b6c862c20df3c2
github_Ryan30551 <patrickryanf@gmail.com>
2022-09-04 13:42:32 -0500

    Final Product

--- scripts.js
+++ scripts.js
@@ -1,154 +1,127 @@
-//************************************************RYAN'S ROAST BEAN********************************************************************************
-//WEEK 5 MENU APP ASSIGNMENT
-
-class Item {
-    constructor(name) {
-    this.name = name;
+class Member {
+    constructor(name, level) {
+        this.name = name;
+        this.level = level;
+    }
+    describe() {
+        return `${this.name} is on the ${this.level}`;
     }
 }
-
-class Order {
+class Group {
     constructor(name) {
         this.name = name;
-        this.items = [];
     }
-}
 
+    addMember(Member) {
+        if (Member instanceof Member) {
+        this.members.push(Member);
+        } else {
+            throw new Error('You can only add an instance of Member. Argument is not a Member: ${Member}');
+        }
+    }
+    describe() {
+            return `${this.name} has ${this.members.length} members.`;
+        }
+    }
 class Menu {
     constructor() {
-        this.orders = [];
-        this.selectedOrder = null;
+        this.groups = [];
+        this.selectedGroup = null;
     }
 
     start() {
-        let selection = this.showMainMenuOptions();
-        while (selection != 0) {
-            switch (selection) {
-            case '1':
-                this.theBrewMenu();
-                break;
-            case '2':
-                this.createYourOrder();
-                break;
-            case '3':
-                this.showYourOrder();
-                break;
-            case '4':
-                this.deleteOrder();
-                break;
-            default:
-                    selection = 0;
-            }
-            selection = this.showMainMenuOptions();
-        }
-
-        alert('Goodbye!');
-    }
-
-    showMainMenuOptions() {
-        return prompt(`
-        0) exit
-        1) Show The Brew Menu
-        2) Create Your Order
-        3) Show Your Order
-        4) Delete Order
-        `);
-    }
-
-    showOrderMenuOptions(orderInfo) {
-        return prompt (`
-        0) Return to Brew Menu
-        1) Add Beverage
-        2) Delete Beverage
-
-        ${orderInfo}
-        `);
-    }
-    theBrewMenu() {
-        alert(`
-        
-                            Menu Options
-        ------------------------------------------------------------------
-        Drink:              Description:
-        -Cappucino          Steamed Milk (mostly foam), espresso
-        -Latte              Steamed Milk (small amount of foam), espresso
-        -Mocha              Steamed Milk, chocolate syrup, espresso
-        -Americano          Espresso, hot water
-        `);
-    }
-
-    createYourOrder() {
-        let name = prompt('What is the name for the order?');
-        this.orders.push(new Order(name));
-        this.showYourOrder();
-        alert('Good choice! We will get to brewing!');
-        //this.addAnItem();
-        console.log('Test');
-        return 6;
-    }
-
-    showYourOrder() {
-        let index = prompt('What is the name for the order?');
-    
-        console.log('Its', index > -1 && index < this.orders.length); //
-
-        if (index > -1 && index < this.orders.length) {
-
-            console.log ('That', this.selectedOrder); //
-
-            console.log('This order', this.orders[index]); //
-
-            this.selectedOrder('This', this.selectedOrder); //
-
-            console.log('This', this.selectedOrder); //
-        
-            let description = 'Order Name: ' + this.selectedOrder.name + '\n';
-
-         for(let i = 0; i < this.selectedOrder.items.length; i++) {
-                description += i + '# ' + this.selectedOrder.items[i].name + '\n';
-        }
-    }
-
-            let selection = this.showOrderMenuOptions(description);
+        let selection = this.showMainMenu();
+        while (selection !=0) {
             switch (selection) {
                 case '1':
-                    this.addAnItem();
+                    this.createGroup();
                     break;
+                case '2': 
+                    this.viewGroup();
+                    break;
+                case '3':
+                    this.deleteGroup();
+                    break;
+                case '4':
+                    this.displayGroups();
+                    break;
+                default:
+                    selection = 0;
+            }
+            selection = this.showMainMenu();
+        }
+
+        alert ('Logged Out')
+    }
+
+    showMainMenu() {
+        return prompt(`
+        0) Exit App
+        1) Create Group
+        2) View Group
+        3) Delete Group
+        4) Display All Groups
+        `);
+    }
+
+   showGroupMemberMenuOptions(groupInfo) {
+    return(`
+    0) Go Back
+    1) Create member
+    2) Delete member
+    ------------------------
+    ${groupInfo}
+    `);
+   }
+
+    displayGroups() {
+        let groupString = '';
+        for(let i =0; i < this.groups.length; i++) {
+            groupString += i + ') ' + this.groups[i].name + '\n';
+        }
+        alert(groupString);
+    }
+
+    createGroup() {
+        let name = prompt('Enter group Name for new group');
+        this.groups.push(new Group(name));
+    }
+
+    viewGroup() {
+        let index = prompt ('Enter index of the group you wish to view:');
+        if (index > -1 && index < this.groups.length) {
+            this.selectedGroup = this.groups[index];
+            let description = 'Group Index:' + this.selectedGroup.name + '\n';
+
+            for(let i = 0; i < this.selectedGroup.name.length; i++) {
+                description += i + ') ' + this.selectedGroup.name[i].name
+                 + '-' + this.selectedGroup.members[i].level + '\n';
+            }
+            let selection = this.showGroupMenuOptions(description);
+            switch(selection) {
+                case '1':
+                this.createMember();
+                break;
                 case '2':
-                    this.deleteAnItem();
-        }
+                this.deleteMember();
 
-    }
-    
-    deleteOrder() {                              
-        let index = prompt('What is the order number you want to delete?');     
-        if (index > -1 && index < this.orders.length) {       
-            this.orders.splice(index, 1);
-            alert('Your order has been deleted. Let us know what we can do better to serve you!');               
+            }
         }
     }
-
-    addAnItem() {            
-        let name = prompt(`What would you like to add to your order?
-        ----------------------------------------------------
-        Menu Items
-        ----------------------------------------------------
-        Drinks:                         Snacks:
-        - Chocobean Latte               - Ifrit's Flaming Sandwich
-        - MP Espresso                   - Titan's Scrumptious Scramble
-        - Cactuar Cold Brew             - Healer Healthy Salad`);     
-                                                                        
-        this.selectedOrder.items.push(new Item(name));
-        return 7;                                                        
+    createMember() {
+        let name = prompt('Enter name for new player');                      //Was unable to get this functionality to work despite try to troubleshoot.
+        let level = prompt('Enter level for new member');
+        this.selectedGroup.members.push(new Member(name, level));
     }
 
-    deleteAnItem() {        
-        let index = prompt('What is the item number you want to delete from this order?');   
-        if(index > -1 && index < this.selectedOrder.items.length) {     
-            this.selectedOrder.items.splice(index, 1);      
+    deleteMember() {
+    let index = prompt('Enter the index of the member you want to delete:');   //Could not get this functionality to work either in my program code
+    if (index > -1 && index < this.selectedGroup.members.length) {
+        this.selectedGroup.splice(index, 1);
+        }
     }
+}
+
 
 let menu = new Menu();
 menu.start();
-    }
-}
